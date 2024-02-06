52d5ff5df96ae906b22ebbdaea6cffb9b3454ea7
DruzhininaNadezhda <wulvjxe@gmail.com>
2024-02-06 11:16:11 +0300

    -

--- src/main/java/Server/ServerTCP.java
+++ src/main/java/Server/ServerTCP.java
@@ -3,6 +3,8 @@ package Server;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ServerTCP {
     public void start() {
@@ -10,6 +12,7 @@ public class ServerTCP {
             while (true) {
                 Socket socket = serverSocket.accept();
                 InputStreamReader readerIn = new InputStreamReader(socket.getInputStream()); // Входной поток
+                System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date().getTime()));
                 char[] buffer = new char[1024];
                 StringBuilder sb = new StringBuilder();
                 for (int i; (i = readerIn.read(buffer, 0, buffer.length)) > 0; ) { //получение и запись сообщения
@@ -17,10 +20,11 @@ public class ServerTCP {
                     System.out.println(sb);
                 }
                 socket.shutdownInput();
-                String answer = "Доставлено сообщение:" + sb.toString();
+
+                System.out.println(socket.getLocalSocketAddress());
+                String answer = "Доставлено сообщение:" + sb+ "\n" + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date().getTime());;
                 OutputStreamWriter writerOut = new OutputStreamWriter(socket.getOutputStream()); //Выходной поток
                 writerOut.write(answer); //запись сообщения в поток
-                System.out.println("Процесс идёт");
                 writerOut.flush(); // очистка буфера
                 socket.shutdownOutput(); // закрывается выходной поток
             }
